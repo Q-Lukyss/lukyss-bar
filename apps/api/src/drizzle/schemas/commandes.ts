450572cf98740fc1f095a317ef5ef485996db446
Q-Lukyss <quentin.lkss@gmail.com>
2026-03-22 20:57:00 +0100

    add :ajout chamos commandes

--- apps/api/src/drizzle/schemas/commandes.ts
+++ apps/api/src/drizzle/schemas/commandes.ts
@@ -1,10 +1,14 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { ulid } from 'ulid';
 
 export const commandes = pgTable('commandes', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => ulid()),
+  customerName: text('customer_name').notNull(),
+  promoCode: text('promo_code').notNull(),
+  publicToken: text('public_token').notNull().unique(),
+  totalPrice: integer('total_price').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at')
     .defaultNow()
