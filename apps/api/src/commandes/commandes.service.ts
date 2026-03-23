0f41ea4945c0db341a741a34d9a00e24745592f2
Q-Lukyss <quentin.wow@free.fr>
2026-03-23 10:45:45 +0100

    fix : fix PENDING et pas pending

--- apps/api/src/commandes/commandes.service.ts
+++ apps/api/src/commandes/commandes.service.ts
@@ -82,7 +82,7 @@ export class CommandesService {
         customerName: dto.customerName,
         promoCode,
         publicToken: this.generatePublicToken(),
-        status: 'pending',
+        status: 'PENDING',
         totalPrice,
       })
       .returning();
