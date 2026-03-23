25726a8e3c2ff8cf4fcc7a9fed400a417215d39f
Q-Lukyss <quentin.wow@free.fr>
2026-03-23 11:26:21 +0100

    add : nestia

---
+++ apps/api/nestia.config.ts
@@ -0,0 +1,10 @@
+import type { INestiaConfig } from '@nestia/sdk';
+
+const config: INestiaConfig = {
+  input: ['./src/commandes/commandes.controller.ts'],
+  output: '../../packages/nestia-sdk/src',
+  // distribute: '../../packages/nestia-sdk',
+  simulate: false,
+};
+
+export default config;
